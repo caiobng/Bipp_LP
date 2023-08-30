a6a0c672f75ee7ae8d934b35ba92a63a3e3fb486
caiobng <caioalmeida@being.com.br>
2023-08-30 08:35:22 -0300

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-window.addEventListener("scroll",function(){
-    let navbarmenu = document.querySelector('#navbar')
-    navbarmenu.classList.toggle('rolagem',window.scrollY > 0)
-})
-
 const handlePhone = (event) => {
     let tel = event.target
     tel.value = phoneMask(tel.value)
@@ -15,3 +10,30 @@ const phoneMask = (value) => {
     value = value.replace(/(\d)(\d{4})$/,"$1-$2")
     return value
 }
+
+window.addEventListener("scroll",function(){
+    let navbarmenu = document.querySelector('.navbarmenu')
+    navbarmenu.classList.toggle('rolagem',window.scrollY > 30)
+})
+
+    document.addEventListener('DOMContentLoaded', function () {
+        var navbarCollapse = document.querySelector('#navbarNavDropdown');
+        var iconOpen = document.querySelector('.navbar-toggler-icon-open');
+        var iconClose = document.querySelector('.navbar-toggler-icon-close');
+
+        navbarCollapse.addEventListener('show.bs.collapse', function () {
+            iconOpen.style.display = 'none';
+            iconClose.style.display = 'block';
+        });
+
+        navbarCollapse.addEventListener('hide.bs.collapse', function () {
+            iconOpen.style.display = 'block';
+            iconClose.style.display = 'none';
+        });
+    });
+
+    $(document).ready(function() {
+        $('.navbar-toggler').click(function() {
+            $('.navbar').toggleClass('expanded');
+        });
+    });
